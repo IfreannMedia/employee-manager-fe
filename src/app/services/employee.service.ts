8777b3c4b4b1aa27305dedb25b4ea92bf91eace7
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-03 13:48:09 +0200

    added service
    with some api methods, and added fields to the employee interface

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Employee } from '../interfaces/employee.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,19 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any> {
-    return this.http.get <any>(`${this.apiServiceUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<any>(`${this.apiServiceUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<any>(`${this.apiServiceUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<any>(`${this.apiServiceUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<any>(`${this.apiServiceUrl}/employee/delete/${employeeId}`);
   }
 }
