c785d16ce3b618e044344a866ea02cb00fdefde4
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-03 15:45:38 +0200

    updated card view
    added ellipsis styling to longer names and job titles, and added a bootsrap tooltip to then display them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
