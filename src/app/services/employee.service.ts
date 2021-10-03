4bbce3c076d117b1600cac553b892ebb6dad4853
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-03 13:33:20 +0200

    Created service
    added method to get all employees, created employee interface, added httpClientModule to app.module

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiServiceUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<any> {
+    return this.http.get <any>(`${this.apiServiceUrl}/employee/all`);
+  }
+}
